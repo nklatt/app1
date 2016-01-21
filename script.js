e8a240bc2c50955f4f837f94ef465dabc20fa637
Nathan Klatt <n8klatt@gmail.com>
2016-01-20 21:01:02 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var colorOne = "blue";
+var colorTwo = "red";
+var nextColor = colorOne;
+$(".board td").on("click", function() {
+  $(this).css("background-color", nextColor);
+  nextColor = nextColor === colorOne ? colorTwo : colorOne;
+});
+$("#reset").on("click", function() {
+  $(".board td").css("background-color", "white");
+});
